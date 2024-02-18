1596c2c9b736200b730d25dea002cd5220d17206
Yashkhetan-04 <yashkhetan04@gmail.com>
2024-02-18 20:12:59 +0530

    Conditional Layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,17 +1,23 @@
+'use client'
 import Link from "next/link"
 import './login.css'
-const Layout = ({children}) => {
-    return(
-        <div >
-            <ul className="login-menu">
-                <li><h3>Login Navbar</h3></li>
-                <li><Link href='/login'>Login Main</Link></li>
-                <li><Link href='/login/loginStudent'>Student Login</Link></li>
-                <li><Link href='/login/loginTeacher'>Teacher Login</Link></li>
-            </ul>
+import { usePathname } from "next/navigation"
+const Layout = ({ children }) => {
+    const pathname = usePathname();
+    return (
+        <div>
+            {
+                pathname !== "/login/loginTeacher" ? 
+                <ul className="login-menu">
+                    <li><h3>Login Navbar</h3></li>
+                    <li><Link href='/login'>Login Main</Link></li>
+                    <li><Link href='/login/loginStudent'>Student Login</Link></li>
+                    <li><Link href='/login/loginTeacher'>Teacher Login</Link></li>
+                </ul> 
+                : null
+            }
             {children}
         </div>
     )
 }
-
 export default Layout
